7b5c667880910481deec6d319ae3aed9e5d39982
DChen <dchen@DChende-MacBook-Pro.local>
2019-10-15 16:00:32 +0800

    don't use dataframe use list of sets to store candidates instead

--- Assignment1/apriori.py
+++ Assignment1/apriori.py
@@ -1,2 +1,10 @@
 import IBMReader
 import KaggleReader
+class Apriori():
+    def __init__(self, DB):
+        '''
+        constructor for Apriori object
+        it should receive a ditctionary which store transaction database
+        '''
+        self.__DB = DB.copy() # shallow copy transaction database
+        self.__Candidates =  list(set())
