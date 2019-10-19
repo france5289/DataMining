ad8e7574df5c15c36bd3d6f57f18d94b2f524b05
france5289 <st980407@gmail.com>
2019-10-19 16:21:34 +0800

    implement ConstructPatternPath()

--- Assignment1/FPTree.py
+++ Assignment1/FPTree.py
@@ -25,15 +25,36 @@ class FPTree():
         '''
         contruct pattern path from root node \n
         if startnode is None then it start from root node \n
+        this function is recursive function with base case when pattern is empty \n
         Parameter: \n
             pattern(lits of sets)
             startnode(Node obj.)
         '''
-        if startnode is None:
+        # base case
+        if len(pattern) == 0:
+            print('Pattern path constructed')
+            return
+        if startnode is None: # start from root
             startnode = self.__root
-        
-            
-                
+        insert_item = pattern[0] # it is a string
+        insert_itemset = {insert_item} # it is a set
+        if startnode.getItem() == insert_itemset:
+            startnode.addItem(insert_itemset)
+            pattern.remove(insert_item)
+            return self.ContructPatternPath(startnode, pattern)
+        else:
+            for child in startnode.getChilds():
+                if child.getItem() == insert_itemset:
+                    child.addItem(insert_itemset)
+                    pattern.remove(insert_item)
+                    return self.ContructPatternPath(child, pattern)
+            # if the following codes are executed
+            # it implies maybe this node don't have any child or its childs' item don't fit insert_itemset
+            # we have to create a new node
+            newnode = Node(item = insert_itemset, supcnt=1, parent=startnode)
+            startnode.addChild(newnode)
+            pattern.remove(insert_item)
+            return self.ContructPatternPath(newnode, pattern)
     #-----------Setter-----------
     
 
