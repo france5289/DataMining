3dc265d85a0a3a50bbc10f2dd044a46538e7c189
france5289 <st980407@gmail.com>
2019-10-19 14:26:19 +0800

    init GetRoot() and ContructPatternPath()

--- Assignment1/FPTree.py
+++ Assignment1/FPTree.py
@@ -3,17 +3,40 @@ class FPTree():
     def __init__(self, node=None):
         '''
         constructor for Tree object
-        it can receive a Node object which will be the root node of the tree
+        it can receive a Node object which will be the root node of the tree \n
         Parameter:
             node(optional)(Node obj.)
-            if node parameter value is None, then it will create a NULL node as root node
+            if node parameter value is None, then it will create a NULL node as root node \n
         Return: none
         '''
-        if type(node) is not None and type(node) is not Node:
-            raise ValueError('Tree constructor just receive Node object as parameter!')
         self.__root = Node() if type(node) is None else node
-    #-----------Setter-----------
+    #-----------Getter-----------
+    def GetRoot(self):
+        '''
+        Return root node of FPTree \n
+        Parameter : none \n
+        Return : Node object \n
+        '''
+        return self.__root
+    #-----------Getter-----------
     
     #-----------Setter-----------
+    def ContructPatternPath(self, startnode, pattern):
+        '''
+        contruct pattern path from root node \n
+        if startnode is None then it start from root node \n
+        Parameter: \n
+            pattern(lits of sets)
+            startnode(Node obj.)
+        '''
+        if startnode is None:
+            startnode = self.__root
+        
+            
+                
+    #-----------Setter-----------
     
-    
+
+if __name__ == '__main__':
+    # FPTree.InsertPattern(parentnode=a.getroot(), {1,2,3})
+    pass
