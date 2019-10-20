1b16cb5fa99052fdbf454d5a2fcfecbb9d5a5e60
france5289 <st980407@gmail.com>
2019-10-21 00:15:09 +0800

    init AssociationRuleMining.py

---
+++ Assignment1/AssociationRuleMining.py
@@ -0,0 +1,9 @@
+import KaggleReader
+import IBMReader
+from FPGrowth import FPGrowth
+from apriori import Apriori
+
+if __name__ == '__main__':
+    KAGGLE_DATA_PATH = 'Assignment1/GroceryStoreDataSet.csv'
+    IBM_DATA_PATH = 'Assignment1/output.data'
+    
