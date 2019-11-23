f65b6afa411303749b2511bf1bfdfa559ad49846
france5289 <st980407@gmail.com>
2019-11-24 01:26:42 +0800

    generate training data frame

--- Assignment2/train.py
+++ Assignment2/train.py
@@ -1,7 +1,9 @@
 from player_data_generator import PlayerDataGenerator
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
-    batting_data = PlayerDataGenerator.Batting_data_Generate()
-    
+    train = PlayerDataGenerator.Batting_data_Generate(player_num=1000)
+    train.drop(['OBP', 'SLG', 'OPS'], axis = 1, inplace = 'True')
+    train = train.to_numpy()
